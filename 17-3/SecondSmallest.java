9c5f8b5d735b2698c0bf189e910b18db4ce9ea39
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-03-17 21:57:11 +0530

    Corrected program-Second Smallest

---
+++ 17-3/SecondSmallest.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+class SecondSmallest
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the size of an array :");
+		int size=sc.nextInt();
+		int ar[]=new int[size];
+		for(int i=0;i<ar.length;i++)
+		{
+			System.out.print("Enter the "+(i+1)+" value :");
+			ar[i]=sc.nextInt();
+		}
+		for(int i=0;i<ar.length;i++)
+		{
+			System.out.print(ar[i]+" ");
+			
+		}
+		System.out.println();
+		int min=ar[0];
+		int second_min=ar[0];
+		for(int i=0;i<ar.length;i++)
+		{
+			if(ar[i]<min)
+			{
+				min=ar[i];
+			}
+		}
+		System.out.println("The minimum value is "+min);
+		for(int i = 0; i < ar.length; i++)
+		{
+			if(ar[i] != min)
+			{
+				second_min = ar[i];
+				break;
+			}
+		}
+		for(int i=0;i<ar.length;i++)
+		{
+			if(ar[i]<second_min && ar[i]!=min)
+			{
+				second_min=ar[i];
+			}
+		}
+		System.out.println("The Second minimum value is "+second_min);
+	}
+}
+		
